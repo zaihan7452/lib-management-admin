8b8361acc1f27e0a1690a3b38b312f9c6ab7b64c
Han Zai <1613710573@qq.com>
2026-04-28 20:23:33 +0800

    feat: Initialize Spring Boot application with basic configuration

---
+++ src/test/java/com/zahan/app/libmgmt/LibmgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zahan.app.libmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibmgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
